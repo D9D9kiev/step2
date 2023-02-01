9e37cef6ae28a6d76d865f5287dd1f52731d2007
Albina11 <albinabasova1994@gmail.com>
2023-02-01 21:42:50 +0200

    menu

--- src/assets/js/components/dropmenu.js
+++ src/assets/js/components/dropmenu.js
@@ -1,10 +1,27 @@
 const hamburger = document.querySelector('.hamburger');
 const hamburgerNav = document.querySelector('.hamburger-nav');
 const hamburgerItems = document.querySelector ('.hamburger-nav__nav-items')
+const wrapper = document.querySelector('.wrapper');
 
 
-hamburger.addEventListener('click', function() {
-  hamburger.classList.toggle('open');
+const toggleMenu = () => {
   hamburgerNav.classList.toggle('active');
+  hamburger.classList.toggle('open');
+}
 
+hamburger.addEventListener('click', e => {
+  e.stopPropagation();
+
+  toggleMenu();
 });
+
+document.addEventListener('click', e => {
+  let target = e.target;
+  let its_menu = target == hamburgerNav || hamburgerNav.contains(target);
+  let its_hamburger = target == hamburger;
+  let menu_is_active = hamburgerNav.classList.contains('active');
+  
+  if (!its_menu && !its_hamburger && menu_is_active) {
+    toggleMenu();
+  }
+})
